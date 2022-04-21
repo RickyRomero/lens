38045e764f63418dfcc446e7a9e431b054294af4
Ricky Romero <ricky.romero@gmail.com>
2022-04-20 18:40:33 -0600

    Add statistics

--- benchmark.js
+++ benchmark.js
@@ -19,6 +19,9 @@ const tableConfig = {
   'AVIF 1920/3': { format: 'avif', width: 1920 },
 }
 
+const filesizeResults = []
+const scoreResults = []
+const bppResults = []
 const runCompression = async () => {
   const qualityCsv = await fs.readFile(
     'quality-tests.csv', { encoding: 'utf8' }
@@ -31,13 +34,17 @@ const runCompression = async () => {
     const num = imageRow.Image
     const padded = num.padStart(3, '0')
     const source = sourcePath(`./Image_${padded}.png`)
-  
+
+    const scoreResult = { image: num }
+    const filesizeResult = { image: num }
+    const bppResult = { image: num }
+
     for (configKey of Object.keys(tableConfig)) {
       const { format, width } = tableConfig[configKey]
       const quality = Number(imageRow[configKey])
       if (!quality) { continue }
 
-      console.log({ padded, format, width, quality })
+      // console.log({ padded, format, width, quality })
       const intermediateScaled = await sharp(source)
         .ensureAlpha()
         .resize({ width })
@@ -46,17 +53,32 @@ const runCompression = async () => {
         .toBuffer()
       const rawCompressed = await compress({ source, format, width, quality })
       const { height } = await intermediateScaled.metadata()
+      const pixelCount = width * height
 
       const scaledPixels = await sharp(rawScaled).ensureAlpha().raw().toBuffer()
       const compressedPixels = await sharp(rawCompressed).ensureAlpha().raw().toBuffer()
       const scaledId = { data: scaledPixels, width, height }
       const compressedId = { data: compressedPixels, width, height }
-      score = ssim(scaledId, compressedId).mssim
+      const score = ssim(scaledId, compressedId).mssim
 
-      console.log(score, rawCompressed.byteLength)
+      const filesize = rawCompressed.byteLength
+      const bpp = (filesize * 8) / pixelCount
+
+      scoreResult[configKey] = score
+      filesizeResult[configKey] = filesize
+      bppResult[configKey] = bpp
+
+      console.log({num, configKey, score, filesize, bpp})
     }
-    
+
+    scoreResults.push(scoreResult)
+    filesizeResults.push(filesizeResult)
+    bppResults.push(bppResult)
   }
+
+  console.log(Papa.unparse(scoreResults))
+  console.log(Papa.unparse(filesizeResults))
+  console.log(Papa.unparse(bppResults))
 }
 
 runCompression()
